79c177eed76878eee8c012afb1a3e210852efab5
1c3Cr4Ck <icecrack7613@gmail.com>
2015-11-24 14:51:32 +0100

    Ajout fonctions

--- Projet.c
+++ Projet.c
@@ -3,32 +3,47 @@
 
 int affichageMenu()
 { 
-     int choixMenu = 0;
-     printf("---Sélectionner votre fonction de chiffrement---\n");
-     printf("1.Chiffrement AES\n");
-     printf("2.Chiffrement El-Gamal\n");
-     printf("3.Signature El-Gamal\n");
-     printf("\nVotre choix?\n");
-     scanf("%d", &choixMenu);
-     return choixMenu;
+    int choixMenu = 0;
+    printf("---Sélectionner votre fonction de chiffrement---\n");
+    printf("1.Chiffrement AES\n");
+    printf("2.Chiffrement El-Gamal\n");
+    printf("3.Signature El-Gamal\n");
+    printf("\nVotre choix?\n");
+    scanf("%d", &choixMenu);
+    return choixMenu;
+}
+
+void chiffrementAES()
+{
+    printf("Vous avez choisi le chiffrement AES!\n\n");
+}
+
+void chiffrementElGamal()
+{
+    printf("Vous avez choisi le chiffrement El-Gamal!\n\n");
+}
+
+void signatureElGamal()
+{
+    printf("Vous avez choisi la signature El-Gamal!\n\n");
 }
 
 int main(int argc, char *argv[])
 {
     switch(affichageMenu())
     {
-                     case 1:
-                          printf("Vous avez choisi le chiffrement AES!\n\n");
-                          break;
-                     case 2:
-                          printf("Vous avez choisi le chiffrement El-Gamal!\n\n");
-                          break;
-                     case 3:
-                          printf("Vous avez choisi la signature El-Gamal!\n\n");
-                          break;    
-                     default:
-                          printf("Choix non valide!\n\n");
-                          break;
+        case 1:
+            chiffrementAES();
+            break;
+        case 2:
+            chiffrementElGamal();
+            break;
+        case 3:
+            signatureElGamal();
+            break;    
+        default:
+            printf("Choix non valide!\n\n");
+            break;
     }
   	return 0;
 }
