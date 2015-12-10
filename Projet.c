eac344d28f9e405df02878e90aa25b9547047aa4
icecr4ck <icecrack7613@gmail.com>
2015-12-10 16:23:21 +0100

    Déchiffrement marche presque

--- Projet.c
+++ Projet.c
@@ -103,13 +103,33 @@ void mixColumns(unsigned char *stateIn, unsigned char *stateOut)
         // Mélange de la dernière colonne
         temp = (3*stateIn[0+i*4]) ^ stateIn[1+i*4] ^ stateIn[2+i*4] ^ (2*stateIn[3+i*4]);
         temp = moduloMixColumns(temp);
-        stateOut[2+i*4]=(char)temp;
+        stateOut[3+i*4]=(char)temp;
     }
 }
 
 void invMixColumns(unsigned char *stateIn, unsigned char *stateOut)
 {
-
+    unsigned char temp='0';
+    for (int i = 0; i < 4; ++i) //Pour chaque ligne
+    {
+        int temp=0;
+        // Mélange de la première colonne
+        temp = (0x0e*stateIn[0+i*4]) ^ (0x0b*stateIn[1+i*4]) ^ (0x0d*stateIn[2+i*4]) ^ (0x09*stateIn[3+i*4]);
+        temp = moduloMixColumns(temp);
+        stateOut[0+i*4]=(char)temp;
+        // Mélange de la deuxième colonne
+        temp = (0x09*stateIn[0+i*4]) ^ (0x0e*stateIn[1+i*4]) ^ (0x0b*stateIn[2+i*4]) ^ (0x0d*stateIn[3+i*4]);
+        temp = moduloMixColumns(temp);
+        stateOut[1+i*4]=(char)temp;
+        // Mélange de la troisième colonne
+        temp = (0x0d*stateIn[0+i*4]) ^ (0x09*stateIn[1+i*4]) ^ (0x0e*stateIn[2+i*4]) ^ (0x0b*stateIn[3+i*4]);
+        temp = moduloMixColumns(temp);
+        stateOut[2+i*4]=(char)temp;
+        // Mélange de la dernière colonne
+        temp = (0x0b*stateIn[0+i*4]) ^ (0x0d*stateIn[1+i*4]) ^ (0x09*stateIn[2+i*4]) ^ (0x0e*stateIn[3+i*4]);
+        temp = moduloMixColumns(temp);
+        stateOut[3+i*4]=(char)temp;
+    }
 }
 
 void shiftRows(unsigned char *stateIn, unsigned char *stateOut)
@@ -182,9 +202,35 @@ void keyExpansion(char *cle)
     }
 }
 
+void invKeyExpansion(char *cle)
+{
+    char sbytesInv[256] =
+    {
+        0x52 ,0x09 ,0x6a ,0xd5 ,0x30 ,0x36 ,0xa5 ,0x38 ,0xbf ,0x40 ,0xa3 ,0x9e ,0x81 ,0xf3 ,0xd7 ,0xfb,
+        0x7c ,0xe3 ,0x39 ,0x82 ,0x9b ,0x2f ,0xff ,0x87 ,0x34 ,0x8e ,0x43 ,0x44 ,0xc4 ,0xde ,0xe9 ,0xcb,
+        0x54 ,0x7b ,0x94 ,0x32 ,0xa6 ,0xc2 ,0x23 ,0x3d ,0xee ,0x4c ,0x95 ,0x0b ,0x42 ,0xfa ,0xc3 ,0x4e,
+        0x08 ,0x2e ,0xa1 ,0x66 ,0x28 ,0xd9 ,0x24 ,0xb2 ,0x76 ,0x5b ,0xa2 ,0x49 ,0x6d ,0x8b ,0xd1 ,0x25,
+        0x72 ,0xf8 ,0xf6 ,0x64 ,0x86 ,0x68 ,0x98 ,0x16 ,0xd4 ,0xa4 ,0x5c ,0xcc ,0x5d ,0x65 ,0xb6 ,0x92,
+        0x6c ,0x70 ,0x48 ,0x50 ,0xfd ,0xed ,0xb9 ,0xda ,0x5e ,0x15 ,0x46 ,0x57 ,0xa7 ,0x8d ,0x9d ,0x84,
+        0x90 ,0xd8 ,0xab ,0x00 ,0x8c ,0xbc ,0xd3 ,0x0a ,0xf7 ,0xe4 ,0x58 ,0x05 ,0xb8 ,0xb3 ,0x45 ,0x06,
+        0xd0 ,0x2c ,0x1e ,0x8f ,0xca ,0x3f ,0x0f ,0x02 ,0xc1 ,0xaf ,0xbd ,0x03 ,0x01 ,0x13 ,0x8a ,0x6b,
+        0x3a ,0x91 ,0x11 ,0x41 ,0x4f ,0x67 ,0xdc ,0xea ,0x97 ,0xf2 ,0xcf ,0xce ,0xf0 ,0xb4 ,0xe6 ,0x73,
+        0x96 ,0xac ,0x74 ,0x22 ,0xe7 ,0xad ,0x35 ,0x85 ,0xe2 ,0xf9 ,0x37 ,0xe8 ,0x1c ,0x75 ,0xdf ,0x6e,
+        0x47 ,0xf1 ,0x1a ,0x71 ,0x1d ,0x29 ,0xc5 ,0x89 ,0x6f ,0xb7 ,0x62 ,0x0e ,0xaa ,0x18 ,0xbe ,0x1b,
+        0xfc ,0x56 ,0x3e ,0x4b ,0xc6 ,0xd2 ,0x79 ,0x20 ,0x9a ,0xdb ,0xc0 ,0xfe ,0x78 ,0xcd ,0x5a ,0xf4,
+        0x1f ,0xdd ,0xa8 ,0x33 ,0x88 ,0x07 ,0xc7 ,0x31 ,0xb1 ,0x12 ,0x10 ,0x59 ,0x27 ,0x80 ,0xec ,0x5f,
+        0x60 ,0x51 ,0x7f ,0xa9 ,0x19 ,0xb5 ,0x4a ,0x0d ,0x2d ,0xe5 ,0x7a ,0x9f ,0x93 ,0xc9 ,0x9c ,0xef,
+        0xa0 ,0xe0 ,0x3b ,0x4d ,0xae ,0x2a ,0xf5 ,0xb0 ,0xc8 ,0xeb ,0xbb ,0x3c ,0x83 ,0x53 ,0x99 ,0x61,
+        0x17 ,0x2b ,0x04 ,0x7e ,0xba ,0x77 ,0xd6 ,0x26 ,0xe1 ,0x69 ,0x14 ,0x63 ,0x55 ,0x21 ,0x0c ,0x7d,
+    };
+    for (int i = 0;i < 4; i++){
+        cle[i] = sbytesInv[(unsigned char)cle[i]];
+    }
+}
+
 void roundChiffrementAES(unsigned char *stateIn, unsigned char *stateOut, char *cle, int Nr)
 {
-    addRoundKey(stateIn, cle);
+    addRoundKey(stateIn, stateOut, cle);
     for (int i=0; i<Nr-1; i++)
     {
         subBytes(stateIn, stateOut);
@@ -198,24 +244,6 @@ void roundChiffrementAES(unsigned char *stateIn, unsigned char *stateOut, char *
     keyExpansion(cle);
     addRoundKey(stateIn, stateOut, cle);
 }
-
-void roundDechiffrementAES(unsigned char *stateIn, unsigned char *stateOut, char *cle, int Nr)
-{
-    addRoundKey(stateIn, cle);
-    for (int i=0; i<Nr-1; i++)
-    {
-        invShiftRows(stateIn, stateOut);
-        invSubBytes(stateIn, stateOut);
-        keyExpansion(cle);
-        addRoundKey(stateIn, stateOut, cle);
-        invMixColumns(stateIn, stateOut);
-    }
-    invShiftRows(stateIn, stateOut);
-    invSubBytes(stateIn, stateOut);
-    keyExpansion(cle);
-    addRoundKey(stateIn, stateOut, cle);
-}
-
 
 void chiffrerAES(int tailleCle, int Nr)
 {
@@ -289,7 +317,7 @@ void chiffrerAES(int tailleCle, int Nr)
     }
 }
 
-void chiffrementAES()
+void choixTailleCleChiffrementAES()
 {
     int choixMenu;
     printf("Vous avez choisi le chiffrement AES !\n\n");
@@ -314,7 +342,121 @@ void chiffrementAES()
         printf("Erreur, taille de clé incorrecte !\n\n");
         break;
     }
+}
 
+void roundDechiffrementAES(unsigned char *stateIn, unsigned char *stateOut, char *cle, int Nr)
+{
+    addRoundKey(stateIn, stateOut, cle);
+    for (int i=0; i<Nr-1; i++)
+    {
+        invShiftRows(stateIn, stateOut);
+        invSubBytes(stateIn, stateOut);
+        invKeyExpansion(cle);
+        addRoundKey(stateIn, stateOut, cle);
+        invMixColumns(stateIn, stateOut);
+    }
+    invShiftRows(stateIn, stateOut);
+    invSubBytes(stateIn, stateOut);
+    invKeyExpansion(cle);
+    addRoundKey(stateIn, stateOut, cle);
+}
+
+void dechiffrerAES(int tailleCle, int Nr)
+{
+    unsigned char stateIn[16];
+    unsigned char stateOut[16];
+    int tailleCleByte = tailleCle/8;
+    FILE* cipher = NULL;
+    FILE* decipher = NULL;
+    cipher = fopen("cipher.txt", "r");
+    decipher = fopen("decipher.txt", "w");
+    char *cle;
+    printf("\n--> Entrez la clé de déchiffrement <--\n");
+    printf("Clé: ");
+    scanf("%s", cle);
+    char normCle[32] = {0xCD, 0x0C, 0x13, 0xEC, 0x5F, 0x97, 0x44, 0x17, 0xC4, 0xA7, 0x7E, 0x3D, 0x64, 0x5D, 0x19, 0x73, 0xE7, 0xC8, 0x37, 0x6D, 0x8D, 0xD5, 0x4E, 0xA9, 0x6C, 0x56, 0xF4, 0xEA, 0x65, 0x7A, 0xAE, 0x08};
+    if (strlen(cle) < tailleCleByte)
+    {
+        int m = strlen(cle);
+        while (strlen(cle) < tailleCleByte)
+        {
+            cle[m] = cle[m-1] ^ normCle[m];
+            m++;
+        }
+    }
+    else if (strlen(cle) > tailleCleByte)
+    {
+        int n = strlen(cle);
+        while (strlen(cle) > tailleCleByte)
+        {
+            cle[n] = 0;
+            n--;
+        }
+    }
+    if (decipher == NULL || cipher == NULL)
+    {
+        printf ("\nErreur dans l'ouverture du fichier !\n\n");
+    }
+    else
+    {
+        while (!feof(cipher))
+        {
+            // On remplit le bloc de 0
+            for (int i=0; i<16; i++)
+            {
+                stateIn[i] = 0;
+            }
+            // Puis on met les valeurs du fichier dans le bloc
+            for (int i=0; i<16; i++)
+            {
+                if (!feof(cipher))
+                {
+                    stateIn[i] = fgetc(cipher);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            // On effectue une round AES avec le bloc en question
+            roundDechiffrementAES(stateIn, stateOut, cle, Nr);
+            // On écrit le bloc chiffré dans un fichier de sortie
+            for (int i=0; i<16; i++)
+            {
+                fputc(stateOut[i], decipher);
+            }
+        }
+        fclose(cipher);
+        fclose(decipher);
+        printf("\nFélicitations, votre fichier a bien été déchiffré !\n\n");
+    }
+}
+
+void choixTailleCleDechiffrementAES()
+{
+    int choixMenu;
+    printf("Vous avez choisi le déchiffrement AES !\n\n");
+    printf("--> Sélectionner la taille de clé <--\n");
+    printf("1.128 bits\n");
+    printf("2.192 bits\n");
+    printf("3.256 bits\n");
+    printf("Votre choix: ");
+    scanf("%d", &choixMenu);
+    switch (choixMenu)
+    {
+    case 1:
+        dechiffrerAES(128,10);
+        break;
+    case 2:
+        dechiffrerAES(192,12);
+        break;
+    case 3:
+        dechiffrerAES(256,14);
+        break;
+    default:
+        printf("Erreur, taille de clé incorrecte !\n\n");
+        break;
+    }
 }
 
 void chiffrementElGamal()
@@ -332,19 +474,23 @@ int main(int argc, char *argv[])
     int choixMenu = 0;
     printf("--> Sélectionner votre fonction de chiffrement <--\n");
     printf("1.Chiffrement AES\n");
-    printf("2.Chiffrement El-Gamal\n");
-    printf("3.Signature El-Gamal\n");
+    printf("2.Déchiffrement AES\n");
+    printf("3.Chiffrement El-Gamal\n");
+    printf("4.Signature El-Gamal\n");
     printf("Votre choix: ");
     scanf("%d", &choixMenu);
     switch(choixMenu)
     {
         case 1:
-            chiffrementAES();
+            choixTailleCleChiffrementAES();
             break;
         case 2:
-            chiffrementElGamal();
+            choixTailleCleDechiffrementAES();
             break;
         case 3:
+            chiffrementElGamal();
+            break;
+        case 4:
             signatureElGamal();
             break;
         default:
